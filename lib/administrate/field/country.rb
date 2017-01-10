8ecb6914feaa1273d852015e3c204e400c47b10c
Marco Arduini <mar.arduini@gmail.com>
2017-01-10 11:11:19 +0100

    Add ::Rails::Engine bit

--- lib/administrate/field/country.rb
+++ lib/administrate/field/country.rb
@@ -1,8 +1,11 @@
 require 'administrate/field/base'
+require 'rails'
 
 module Administrate
   module Field
     class Country < Administrate::Field::Base
+      class Engine < ::Rails::Engine; end
+
       def to_s
         data
       end
