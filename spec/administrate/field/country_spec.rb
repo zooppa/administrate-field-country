0d022436030c1b5610cecbfd74bd6b0ead7eb84a
Michele Gerarduzzi <michelegera@users.noreply.github.com>
2018-09-06 10:19:39 +0200

    Add basic test

--- spec/administrate/field/country_spec.rb
+++ spec/administrate/field/country_spec.rb
@@ -1,4 +1,14 @@
 require "spec_helper"
 
 describe Administrate::Field::Country do
+  subject { Administrate::Field::Country.new(:field, data, :show) }
+
+  describe '#to_partial_path' do
+    let(:data) { true }
+
+    it 'returns a partial based on the page being rendered' do
+      path = subject.to_partial_path
+      expect(path).to eq('/fields/country/show')
+    end
+  end
 end
